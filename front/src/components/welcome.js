7db4070c02e6bc780e663cdb4c7de31498503939
lucasp149 <lucasp149@gmail.com>
2023-01-18 13:20:34 -0300

    pantalla de login

--- front/src/components/welcome.js
+++ front/src/components/welcome.js
@@ -1,21 +1,35 @@
-import { useNavigate } from "react-router-dom"
-
+import { useNavigate} from "react-router-dom";
+import { useState } from "react";
+import LogIn from "./login/logIn";
+import styles from "./styles.module.css";
 
 
 export default function Welcome() {
-const nav = useNavigate();
+    const nav = useNavigate();
+
+    const [logged, setLogged] = useState();
 
 
-function handleClick(to){
-    nav(to);
-}
+    function handleClick(to) {
+        nav(to);
+    }
 
     return (
-        <div>
-            <h1  >WELCOME</h1>
-            <h2 onClick={()=>handleClick("/sales")} >NUEVA VENTA</h2>
-            <h2 onClick={()=>handleClick("/products")} >PRODUCTOS</h2>
-            <h2 onClick={()=>handleClick("/stats")} >ESTADÍSTICAS</h2>
+        <div className={styles.centered}>
+            {
+                logged ?
+
+                    <div>
+                        < h1 > WELCOME</h1 >
+                        <h2 onClick={() => handleClick("/sales")} >NUEVA VENTA</h2>
+                        <h2 onClick={() => handleClick("/products")} >PRODUCTOS</h2>
+                        <h2 onClick={() => handleClick("/stats")} >ESTADÍSTICAS</h2>
+                    </div >
+
+                    :
+                    <LogIn logged={logged} setLogged={setLogged}/>
+            }
         </div>
+
     )
 }
