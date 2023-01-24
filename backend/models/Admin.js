1a2f0c7c9d317726c85e89804051dfddb14fda46
L. Lino Mattos <limbertlino@gmail.com>
2023-01-24 11:14:56 -0400

    Generacion de credenciales con email y password

---
+++ backend/models/Admin.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose")
+
+const bcrypt = require("bcrypt")
+
+// Admin schema
+const AdminSchema = new Schema({
+  name: {
+    type: String,
+    default: "admin"
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  password_hash: {
+    type: String,
+    required: true,
+  },
+})
+
+// Generate Password Hash
+
+AdminSchema.methods.createHash = async plainTextPassword => {
+  try {
+    const saltRounds = 10
+    const salt = await bcrypt.genSalt(saltRounds)
+    return await bcrypt.hash(plainTextPassword, salt)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+// Validate password
+
+// AdminSchema.methods.createHash = async (adminPassword, password_hash) => {
+//   const result = await bcrypt.compare(adminPassword, password_hash)
+//   return result
+
+//   try {
+//   } catch (error) {}
+// }
+
+AdminSchema.methods.hello = async () => {
+  console.log("holaaa")
+}
+
+module.exports.Admin = model("Admin", AdminSchema, "admin")
