411856496f6fec4108f79519e6bdb649700a77fe
simon codes <simont.codes@gmail.com>
2023-01-31 12:01:23 -0500

    add schema for sales as well as the routes

---
+++ backend/models/Sale.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const SalesSchema = new Schema({
+  products: [
+    {
+      productId: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  total: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    select: false,
+    immutable: true,
+  },
+});
+
+module.exports = mongoose.model("Sales", SalesSchema);
---
+++ backend/routes/SalesRoutes.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const router = express.Router();
+const Sales = require("../models/Sales");
+
+// Create a sale
+router.post("/", async (req, res) => {
+  const { products, total } = req.body;
+
+  try {
+    const sale = new Sales({ products, total });
+    await sale.save();
+    res.status(201).json(sale);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Get all sales
+router.get("/", async (req, res) => {
+  try {
+    const sales = await Sales.find().populate("products.productId");
+    res.json(sales);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get one sale
+router.get("/:id", async (req, res) => {
+  try {
+    const sale = await Sales.findById(req.params.id).populate(
+      "products.productId"
+    );
+    if (!sale) {
+      return res.status(404).json({ message: "Sale not found" });
+    }
+    res.json(sale);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Update a sale
+router.patch("/:id", async (req, res) => {
+  try {
+    const sale = await Sales.findById(req.params.id);
+    if (!sale) {
+      return res.status(404).json({ message: "Sale not found" });
+    }
+
+    const { products, total } = req.body;
+    sale.products = products;
+    sale.total = total;
+    await sale.save();
+    res.json(sale);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete a sale
+router.delete("/:id", async (req, res) => {
+  try {
+    const sale = await Sales.findById(req.params.id);
+    if (!sale) {
+      return res.status(404).json({ message: "Sale not found" });
+    }
+    await sale.remove();
+    res.json({ message: "Sale deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
