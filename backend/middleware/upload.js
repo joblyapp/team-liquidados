b1195c787c08fc1adfa6dd52c1531ee686659e86
lucasp149 <lucasp149@gmail.com>
2023-03-23 13:02:15 -0300

    Recieving simons changes

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,11 +1,18 @@
 const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/");
   },
+  // filename: function (req, file, cb) {
+  //   cb(null, file.originalname);
+  // },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const fileExtension = path.extname(file.originalname);
+    const fileName = file.fieldname + "-" + uniqueSuffix + fileExtension;
+    cb(null, fileName);
   },
 });
 
