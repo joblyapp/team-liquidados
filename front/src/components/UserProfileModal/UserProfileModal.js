8fe705dd26f0e4333ada44804799bd58fb5ec36a
lucasp149 <lucasp149@gmail.com>
2023-05-18 13:26:17 -0300

    Parenthesis added

--- front/src/components/UserProfileModal/UserProfileModal.js
+++ front/src/components/UserProfileModal/UserProfileModal.js
@@ -57,114 +57,114 @@ function UserProfileModal(props) {
               headers: {
                 "Content-Type": 'application/json',
               },
-            }
-              .then((res) => {
-                // handle success response
-                /*
-                console.log(res.data);
-                sessionStorage.setItem("name", res.data.name);
-                sessionStorage.setItem("email", res.data.email);
-                sessionStorage.setItem(
-                  "avatar",
-                  res.data.image
-                );
-                */
-                props.onRequestClose();
-              })
-          );
-      })
-  };
+            })
+          .then((res) => {
+            // handle success response
+            /*
+            console.log(res.data);
+            sessionStorage.setItem("name", res.data.name);
+            sessionStorage.setItem("email", res.data.email);
+            sessionStorage.setItem(
+              "avatar",
+              res.data.image
+            );
+            */
+            props.onRequestClose();
+          })
+        });
+  }
+};
 
-  const customStyles = {
-    content: {
-      borderRadius: "10px",
-      width: "25%",
-      margin: "auto auto",
-      height: "65vh",
-      overflow: "hidden",
-    },
-  };
+const customStyles = {
+  content: {
+    borderRadius: "10px",
+    width: "25%",
+    margin: "auto auto",
+    height: "65vh",
+    overflow: "hidden",
+  },
+};
 
-  return (
-    <Modal
-      isOpen={props.isOpen}
-      onRequestClose={props.onRequestClose}
-      contentLabel="Edit Profile Modal"
-      style={customStyles}
-      overlayClassName="Overlay"
+return (
+  <Modal
+    isOpen={props.isOpen}
+    onRequestClose={props.onRequestClose}
+    contentLabel="Edit Profile Modal"
+    style={customStyles}
+    overlayClassName="Overlay"
+  >
+    <h2>Editar Perfil</h2>
+    <form
+      method="post"
+      className="form"
+      encType="multipart/form-data"
+      onSubmit={handleSubmit}
     >
-      <h2>Editar Perfil</h2>
-      <form
-        method="post"
-        className="form"
-        encType="multipart/form-data"
-        onSubmit={handleSubmit}
-      >
-        <label className="profile-picture-container ">
-          <div className="profile-picture">
-            {image ? (
-              image.includes("localhost:8080") ? (
-                <img className="avatar" src={image} />
-              ) : (
-                <img className="avatar" src={image} />
-              )
+      <label className="profile-picture-container ">
+        <div className="profile-picture">
+          {image ? (
+            image.includes("localhost:8080") ? (
+              <img className="avatar" src={image} />
             ) : (
-              <img className="user-icon" src={UserIcon} alt="User Icon" />
-            )}
-          </div>
-          <button
-            type="button"
-            className="custom-file-upload"
-            onClick={() => {
-              document.getElementById("imageProfile").click();
-            }}
-          >
-            <img
-              style={{ marginRight: "5px" }}
-              src={EditIcon}
-              alt="icono editar"
-            />
-            Editar Foto{" "}
-          </button>
-
-          <input
-            onChange={handleImageChange}
-            name="imageProfile"
-            id="imageProfile"
-            type="file"
-          />
-        </label>
-        <label className="inputs">
-          Nombre y apellido:
-          <input
-            className="input"
-            type="text"
-            value={name}
-            onChange={handleNameChange}
-          />
-        </label>
-        <label className="inputs">
-          Email:
-          <input
-            className="input"
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-          />
-        </label>
-        <div className="action-buttons">
-          <button
-            className="cancelar"
-            type="button"
-            onClick={props.onRequestClose}
-          >
-            Cancelar
-          </button>
-          <button type="submit">Guardar</button>
+              <img className="avatar" src={image} />
+            )
+          ) : (
+            <img className="user-icon" src={UserIcon} alt="User Icon" />
+          )}
         </div>
-      </form>
-    </Modal>
-  );
-}
+        <button
+          type="button"
+          className="custom-file-upload"
+          onClick={() => {
+            document.getElementById("imageProfile").click();
+          }}
+        >
+          <img
+            style={{ marginRight: "5px" }}
+            src={EditIcon}
+            alt="icono editar"
+          />
+          Editar Foto{" "}
+        </button>
+
+        <input
+          onChange={handleImageChange}
+          name="imageProfile"
+          id="imageProfile"
+          type="file"
+        />
+      </label>
+      <label className="inputs">
+        Nombre y apellido:
+        <input
+          className="input"
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+        />
+      </label>
+      <label className="inputs">
+        Email:
+        <input
+          className="input"
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </label>
+      <div className="action-buttons">
+        <button
+          className="cancelar"
+          type="button"
+          onClick={props.onRequestClose}
+        >
+          Cancelar
+        </button>
+        <button type="submit">Guardar</button>
+      </div>
+    </form>
+  </Modal>
+);
+
 
 export default UserProfileModal;
